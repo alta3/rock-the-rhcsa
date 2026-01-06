2c82f5f4e4fa31228ba982457ec20a0433d22a25
Chad Feeser <31425191+csfeeser@users.noreply.github.com>
2026-01-06 12:14:32 -0500

    Refactor user check conditions and add password validation

--- check/create_users_check.sh
+++ check/create_users_check.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-# Check users and their conditions
-if id natasha &>/dev/null && id harry &>/dev/null && id sarah &>/dev/null &&
+check_password() {
+  local user="$1"
+  echo "nerdzrule" | su - "$user" -c true &>/dev/null
+}
+
+if id natasha &>/dev/null &&
+   id harry &>/dev/null &&
+   id sarah &>/dev/null &&
    [[ $(id -Gn natasha) == *"starwars"* ]] &&
    [[ $(id -Gn harry) == *"starwars"* ]] &&
    [[ $(id -Gn sarah) == *"startrek"* ]] &&
    [[ $(id -u harry) -eq 2000 ]] &&
-   ([[ $(getent passwd sarah | cut -d: -f7) == "/sbin/nologin" ]] || [[ $(getent passwd sarah | cut -d: -f7) == "/usr/sbin/nologin" ]]); then
-    echo -e "\e[32mSUCCESS!\e[0m"
+   ([[ $(getent passwd sarah | cut -d: -f7) == "/sbin/nologin" ]] || [[ $(getent passwd sarah | cut -d: -f7) == "/usr/sbin/nologin" ]]) &&
+   check_password natasha &&
+   check_password harry &&
+   check_password sarah; then
+
+  echo -e "\e[32mSUCCESS!\e[0m"
 else
-    echo -e "\e[31mNO PASS - TRY AGAIN!\e[0m"
+  echo -e "\e[31mNO PASS - TRY AGAIN!\e[0m"
 fi
